b8023463b9a55a7cde8553fe7cf6141bdb736e51
Roman Yehorov <roman.yehorov@student.tuke.sk>
2024-07-06 01:05:07 +0200

    add quest game page

---
+++ src/main/java/com/textquest/QuestServlet.java
@@ -0,0 +1,37 @@
+package com.textquest;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "QuestServlet", value = "/textquest/quest")
+public class QuestServlet extends HttpServlet {
+    private static final String QUEST_JSP = "/quest.jsp";
+    private static final String RESULTS_PAGE = "/textquest/results";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(true);
+
+        Questions questions = (Questions) session.getAttribute("questions");
+
+        String question = session.getAttribute("question").toString();
+        String answer = req.getParameter("answer");
+
+        String nextQuestion = questions.getNextQuestion(question, answer);
+        session.setAttribute("question", nextQuestion);
+        if (nextQuestion.contains("Перемога") || nextQuestion.contains("Поразка")) {
+            resp.sendRedirect(RESULTS_PAGE);
+            return;
+        }
+
+        session.setAttribute("first_answer", questions.getFirstAnswer(nextQuestion));
+        session.setAttribute("second_answer", questions.getSecondAnswer(nextQuestion));
+
+        getServletContext().getRequestDispatcher(QUEST_JSP).forward(req, resp);
+    }
+}
