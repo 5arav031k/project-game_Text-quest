4782ad21227bb561bd15874767b3a98c35d4e61d
Roman Yehorov <roman.yehorov@student.tuke.sk>
2024-07-06 22:58:33 +0200

    added logs into QuestServlet

--- src/main/java/com/textquest/QuestServlet.java
+++ src/main/java/com/textquest/QuestServlet.java
@@ -1,5 +1,8 @@
 package com.textquest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,16 +13,16 @@ import java.io.IOException;
 
 @WebServlet(name = "QuestServlet", value = "/textquest/quest")
 public class QuestServlet extends HttpServlet {
-
+    private static final Logger LOGGER = LogManager.getLogger(QuestServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession(true);
         if (session.getAttribute("username") == null) {
+            LOGGER.debug("User not logged in");
             resp.sendRedirect(PagePaths.START_PAGE);
             return;
         }
-
         try {
             Questions questions = (Questions) session.getAttribute("questions");
             String question = (String) session.getAttribute("question");
@@ -28,6 +31,7 @@ public class QuestServlet extends HttpServlet {
 
             if (nextQuestion.contains("Перемога") || nextQuestion.contains("Поразка")) {
                 session.setAttribute("result", nextQuestion);
+                LOGGER.debug("opening results page");
                 resp.sendRedirect(PagePaths.RESULTS_PAGE);
                 return;
             }
@@ -36,10 +40,10 @@ public class QuestServlet extends HttpServlet {
             session.setAttribute("first_answer", questions.getFirstAnswer(nextQuestion));
             session.setAttribute("second_answer", questions.getSecondAnswer(nextQuestion));
         } catch (Exception e) {
+            LOGGER.error("Bad session",e);
             resp.sendRedirect(PagePaths.START_PAGE);
             return;
         }
-
         getServletContext().getRequestDispatcher(PagePaths.QUEST_JSP).forward(req, resp);
     }
 }
